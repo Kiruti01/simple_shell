35a227e5c9e8d2b42853c4762f7b9e45c4fcd51d
Kiruti01 <mikekirutic@gmail.com>
2023-07-23 23:55:22 +0300

    shell task 2

--- shell.c
+++ shell.c
@@ -6,11 +6,13 @@
 #include <sys/wait.h>
 
 #define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
 
 void run_shell()
 {
-    char command[MAX_COMMAND_LENGTH];
-    char prompt[] = "simple_shell> ";
+    char input[MAX_COMMAND_LENGTH];
+    char *arguments[MAX_ARGUMENTS + 1[;MAX_COMMAND_LENGTH];
+    char prompt[] = "simple_shell :) -> ";
 
     while (1)
     {
@@ -18,7 +20,7 @@ void run_shell()
         write(STDOUT_FILENO, prompt, strlen(prompt));
 
         /*Read the command from the user*/
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        if (fgets(input, MAX_COMMAND_LENGTH, stdin) == NULL)
         {
             /* Handle end of file (Ctrl+D)*/
             printf("\n");
@@ -26,7 +28,19 @@ void run_shell()
         }
 
         /* Remove the trailing newline character*/
-        command[strcspn(command, "\n")] = '\0';
+        input[strcspn(command, "\n")] = '\0';
+
+        /* Tokenize input into command and arg*/
+        int argc = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL && argc < MAX_ARGUMENTS)
+        {
+            arguments[argc] = token;
+            argc++;
+            token = strtok(NULL, " ");
+        }
+
+        arguments[argc] = NULL; /* terminate arg array*/
 
         /* Fork a new process to execute the command*/
         pid_t pid = fork();
@@ -39,10 +53,10 @@ void run_shell()
         else if (pid == 0)
         {
             /*Child process: execute the command*/
-            if (execlp(command, command, (char *)NULL) == -1)
+            if (execvp(arguments[0], arguments) == -1)
             {
-                // Command not found or execution error*/
-                perror("execlp");
+                /*Command not found or execution error*/
+                perror("execvp");
                 exit(EXIT_FAILURE);
             }
         }
@@ -50,6 +64,7 @@ void run_shell()
         {
             /*Parent process: wait for the child to complete*/
             int status;
+
             waitpid(pid, &status, 0);
         }
     }

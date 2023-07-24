64eab2263e49ded9155f1ada883b2930a61fd3e2
Kiruti01 <mikekirutic@gmail.com>
2023-07-24 13:27:43 +0300

    update

--- shell.c
+++ shell.c
@@ -39,24 +39,78 @@ void execute_command(char *command, char *args[])
         else if (pid == 0)
         {
             /*Child process*/
-            /* check if cmd is '/bin/ls'*/
-            if (strcmp(command, "/bin/ls") == 0)
-            {
-                execve(command, args, NULL);
-            }
-            else
-            {
-                char error_msg[100];
-                snprintf(error_msg, sizeof(error_msg), "Command not found: %s\n", command);
-                write(STDERR_FILENO, error_msg, strlen(error_msg));
-                _exit(EXIT_FAILURE);
-            }
+            execve(command, args, NULL);
+
+            char error_msg[100];
+            snprintf(error_msg, sizeof(error_msg), "Execution failed: %s\n", command);
+            write(STDERR_FILENO, error_msg, strlen(error_msg));
+            _exit(EXIT_FAILURE);
         }
         else
         {
-            /* Parent process*/
+            /* Parent process */
             int status;
-
             wait(&status);
         }
     }
+    else
+    {
+        /* Command not found in the current directory, try the PATH */
+        char *path = getenv("PATH");
+        if (path != NULL)
+        {
+            char *path_copy = strdup(path);
+            char *dir = strtok(path_copy, ":");
+
+            while (dir != NULL)
+            {
+                char *full_path = malloc(strlen(dir) + strlen(command) + 2);
+                sprintf(full_path, "%s/%s", dir, command);
+
+                if (access(full_path, X_OK) == 0)
+                {
+                    /* The command exists and is executable in this directory */
+                    pid_t pid = fork();
+
+                    if (pid < 0)
+                    {
+                        write(STDERR_FILENO, "Fork Failed.\n", 13);
+                        free(full_path);
+                        free(path_copy);
+                        return;
+                    }
+                    else if (pid == 0)
+                    {
+                        /* Child process */
+                        execve(full_path, args, NULL);
+
+                        char error_msg[100];
+                        snprintf(error_msg, sizeof(error_msg), "Execution failed: %s\n", command);
+                        write(STDERR_FILENO, error_msg, strlen(error_msg));
+                        free(full_path);
+                        free(path_copy);
+                        _exit(EXIT_FAILURE);
+                    }
+                    else
+                    {
+                        /* Parent process */
+                        int status;
+                        wait(&status);
+                        free(full_path);
+                        free(path_copy);
+                        return;
+                    }
+                }
+
+                free(full_path);
+                dir = strtok(NULL, ":");
+            }
+
+            free(path_copy);
+        }
+
+        char error_msg[100];
+        snprintf(error_msg, sizeof(error_msg), "Command not found: %s\n", command);
+        write(STDERR_FILENO, error_msg, strlen(error_msg));
+    }
+}

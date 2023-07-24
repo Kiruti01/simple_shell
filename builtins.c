bd9886250c09500e8efc0778753e53a120ee3441
Kiruti01 <mikekirutic@gmail.com>
2023-07-24 21:41:56 +0300

    update betty

--- builtins.c
+++ builtins.c
@@ -1,6 +1,10 @@
 #include "builtins.h"
 #include <unistd.h>
 #include <stdio.h>
+/**
+ * execute_env - Fnctn print crnt env vars.
+ * @envp: Pointer to array of strs cntng env vars.
+*/
 
 void execute_env(char **envp)
 {
@@ -8,9 +12,10 @@ void execute_env(char **envp)
 	for (int i = 0; envp[i] != NULL; i++)
 	{
 		size_t length = 0;
+
 		while (envp[i][length] != '\0')
 			length++;
 		write(STDOUT_FILENO, envp[i], length);
 		write(STDOUT_FILENO, "\n", 1);
 	}
-}	    
+}

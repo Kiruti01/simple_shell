a7a509277213805ea38e6b60eaae1f92406591c3
Kiruti01 <mikekirutic@gmail.com>
2023-07-25 00:32:11 +0300

    header update

--- main.h
+++ main.h
@@ -5,7 +5,9 @@
 
 /**
  * get_char - put a character into a memory block allocated using malloc
- * 
+ * The allocated memory should has two bytes: the character, and the
+ * terminating null byte.
+ *
  * @c: character
  *
  * Return: pointer to that memory block, NULL if error
@@ -13,44 +15,47 @@
 char *get_char(char c);
 
 /**
- * get_string - put str into a memblock allocated using malloc
+ * get_string - put a string into a memory block allocated using malloc
  * @s: string
+ *
  * Return: pointer to that memory block, NULL if error
  */
 char *get_string(char *s);
 
 /**
- * put_number - put int into block of mem
+ * put_number - put an integer into a block of memory
  * @n: integer
- * Return: pointer to mem block, NULL if error
+ *
+ * Return: pointer to that memory block, NULL if error
  */
 char *get_number(int n);
 
 /**
- * print_arg - put one passed in param into a block of mem
- * @type: type of the param
- * Return: pointer memblock, NULL if error
+ * print_arg - put one passed in parameter into a block of memory
+ * @type: type of the parameter
+ *
+ * Return: pointer to the memory block, NULL if error
  */
 char *get_arg(char type, ...);
 
 /**
- * get_binary - convts an unsigned int to bin
+ * get_binary - converts an unsigned int to binary
  * @n: integer
  *
- * Return: pointer to bin
+ * Return: pointer to binary
  */
 char *get_binary(unsigned int n);
 
 /**
- * get_rev - reverse a str
+ * get_rev - reverse a string
  * @s: string
- * Return: str reversed
+ * Return: string reversed
  */
 char *get_rev(char *s);
 
 /**
- * get_rot13 - rot13 as a str
- * @s: str
+ * get_rot13 - rot13 as a string
+ * @s: string
  * Return: rot13
  */
 char *get_rot13(char *s);

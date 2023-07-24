bdad18cfa4c338158484443abe6cb1a15ab2328f
Kiruti01 <mikekirutic@gmail.com>
2023-07-25 00:28:35 +0300

    alias update

--- _alias.c
+++ _alias.c
@@ -2,7 +2,15 @@
 #include "shell.h"
 #include "main.h"
 #include "lists.h"
-
+/**
+ * _alias - Implements the "alias" shell command.
+ * @params: Pointer to a param_t struct with the shell state and args.
+ *
+ * This function defines new aliases or prints existing aliases in the shell.
+ * If called with no args, prints all existing aliases.
+ * If called with args, assumes each arg is an alias name or definition,
+ * and performs the appropriate action.
+ */
 void _alias(param_t *params)
 {
 	unsigned int i;
@@ -22,6 +30,18 @@ void _alias(param_t *params)
 	}
 }
 
+/**
+ * set_alias - creates a new alias or updates an existing one
+ * @name: the name of the alias to set
+ * @params: shell state
+ *
+ * This function sets a new alias or updates an existing one in the shell.
+ * An alias is a command or group of commands that can be referred to by
+ * a name. For example, "ll" can be an alias for "ls -l". The alias is
+ * stored as a key-value pair in the shell's list of aliases.
+ *
+ * Return: void
+ */
 void set_alias(char *name, param_t *params)
 {
 	char *val, *tmp = NULL;
@@ -39,7 +59,7 @@ void set_alias(char *name, param_t *params)
 			if (tmp[1] != '\0')
 			{
 				while (tmp[j] &&
-						(tmp[j] == ' ' || tmp[j] == '\t' || tmp[j] == '\n'))
+					(tmp[j] == ' ' || tmp[j] == '\t' || tmp[j] == '\n'))
 					j++;
 				if (tmp[j] != '\0')
 					set_alias(&tmp[j], params);
@@ -56,6 +76,19 @@ void set_alias(char *name, param_t *params)
 	name[i] = '\0'; /* set = to terminater */
 	make_alias(name, val, params);
 }
+
+/**
+ * make_alias - creates a new alias and adds it to the alias list
+ * @name: name of the alias
+ * @val: value of the alias
+ * @params: parameter struct containing the alias list
+ *
+ * This function creates a new alias using the given name and value,
+ * and adds it to the alias list stored in the parameter struct.
+ * If the alias list is empty, the new alias becomes the first node in the list
+ * Otherwise, the new alias is appended to the end
+ * of the existing list.
+ */
 
 void make_alias(char *name, char *val, param_t *params)
 {

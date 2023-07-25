9a5807bf061a81ac258dc1bb220b48621f20c03d
Kiruti01 <mikekirutic@gmail.com>
2023-07-25 04:13:00 +0300

    strtok

--- tests/strtok.c
+++ tests/strtok.c
@@ -18,6 +18,55 @@ int isDelim(char c, char *delim)
 	}
 	return (0);
 }
+
+/**
+ * _find_end_of_token - Finds the end of a token
+ * @modifier: starting point to search
+ * @delim: delimiters for tokens
+ *
+ * Return: pointer to the end of the token
+ */
+static char *_find_end_of_token(char *modifier, char *delim)
+{
+	int quoteFound = 0;
+
+	while (*modifier)
+	{
+		if (*modifier == '\'')
+			quoteFound = 1;
+
+		if (isDelim(*modifier, delim) && quoteFound == 0)
+			break;
+
+		modifier++;
+	}
+
+	return (modifier);
+}
+
+/**
+ * _handle_single_quote - Handles single quotes in the input
+ * @ptr: pointer to the starting quote
+ * @savePtr: state
+ *
+ * Return: token enclosed in single quotes
+ */
+static char *_handle_single_quote(char *ptr, char **savePtr)
+{
+	ptr++;
+	char *modifier = _find_end_of_token(ptr, "\'");
+
+	if (!modifier)
+	{
+		_printf("no matching quote found!\n");
+		exit(-1);
+	}
+
+	*modifier = '\0';
+	*savePtr = modifier + 1;
+	return (_strdup(ptr));
+}
+
 /**
  * _strtok - strtok_r
  * @str: str to pass
@@ -28,8 +77,7 @@ int isDelim(char c, char *delim)
  */
 char *_strtok(char *str, char *delim, char **savePtr)
 {
-	char *ptr, *modifier, *end;
-	int quoteFound = 0;
+	char *ptr, *end;
 
 	if (*savePtr)
 		ptr = *savePtr;
@@ -46,30 +94,15 @@ char *_strtok(char *str, char *delim, char **savePtr)
 		return (NULL);
 	}
 	if (*ptr == '\'')
-	{
-		ptr++;
-		modifier = _strchr(ptr, '\'');
-		if (!modifier)
-		{
-			_printf("no matching quote found!\n");
-			exit(-1);
-		}
-		*modifier = '\0';
-		*savePtr = modifier + 1;
-		return (_strdup(ptr));
-	}
-	while (*modifier)
-	{
-		if (*modifier == '\'')
-			quoteFound = 1;
-		if (isDelim(*modifier, delim) && quoteFound == 0)
-			break;
-		modifier++;
-	}
+	return (_handle_single_quote(ptr, savePtr));
+
+	char *modifier = _find_end_of_token(ptr, delim);
+
 	if (*modifier == '\0')
 		*savePtr = modifier;
 	else
 		*savePtr = modifier + 1;
+
 	*modifier = '\0';
 	return (_strdup(ptr));
 }
